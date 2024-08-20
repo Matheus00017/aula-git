311f6222270ce5a68dd9bf171dc529ccbe865fca
Matheus00017 <vidalmatheus321@gmail.com>
2024-08-19 22:26:40 -0300

    produtos 19-08

---
+++ produto.js
@@ -0,0 +1,10 @@
+//solicite o nome do produto
+//solicite a quantidade de vendida
+//solicite o valor do produto
+//apresente o valor total
+//solicite a % de desconto
+//apresente o r$ totqal de desconto
+//apresente o valor total da venda depois do desconto
+
+var = parseInt(prompt("Digite o nome do produto "));
+var = valor parseFloat(prompt("digite o valor do produto: " ));
