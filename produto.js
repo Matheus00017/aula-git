d64620a40c0f9b11a0c89992dd40ebbf8bff95b5
Matheus00017 <vidalmatheus321@gmail.com>
2024-08-23 15:06:50 -0300

    produto

--- produto.js
+++ produto.js
@@ -3,8 +3,18 @@
 //solicite o valor do produto
 //apresente o valor total
 //solicite a % de desconto
-//apresente o r$ totqal de desconto
+//apresente o r$ total de desconto
 //apresente o valor total da venda depois do desconto
 
-var = parseInt(prompt("Digite o nome do produto "));
-var = valor parseFloat(prompt("digite o valor do produto: " ));
+var nomeProduto = prompt("Digite o nome do produto ");//coca cola
+var quantidadeVendia = prompt("Digie a quantidade do produto");//5
+var valorProduto = parseFloat(prompt("digite o valor do produto: " )); //50,00
+var valorTotal = quantidadeVendida * valorProduto;//5 * 50,00 
+console.log(("o valor total deu" + valorTotal));//250,00
+var porcentagemDescontoesconto = prompt(("degite o valor da % de desconto"));
+var porcentagemDecimal =porcentagemDesconto / 100
+var descontoReais = valorTotal * porcentagemDecimal;
+console.log("o desconto em R$ doi de " + descontoReais);
+var descontoTotal = valorTotal - descontoReais;
+console.log("o produto teve um total de " + descontoTotal);
+
