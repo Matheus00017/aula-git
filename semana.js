e0cd092ff1ca61cde78793b20b9f638c435b8df8
Matheus00017 <vidalmatheus321@gmail.com>
2024-09-02 22:29:59 -0300

    mes  ano

---
+++ semana.js
@@ -0,0 +1,25 @@
+var dia = parseInt(prompt("Digite o numero que corresponde o dia da semana:"));
+
+
+
+if(dia == 1){
+    console.log("hoje é domingo");
+}else if (dia == 2){
+    console.log("hoje é segunda");
+}else if (dia == 3){
+    console.log("hoje é terça");
+}else if (dia == 4){
+    console.log("hoje é quarta");
+}else if (dia == 5){
+    console.log("hoje é quinta");
+}else if (dia == 6){
+    console.log("hoje é sexta");
+}else if (dia == 7){
+    console.log("hoje é sabado");
+}else if (dia > 7){
+    console.log("esse dia não existe")
+}
+
+
+
+
