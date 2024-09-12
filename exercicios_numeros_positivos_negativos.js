4e0c30eb97003de244a354563cdd97054025f8fb
Matheus00017 <vidalmatheus321@gmail.com>
2024-09-12 14:43:49 -0300

    numero positivos negativo

---
+++ exercicios_numeros_positivos_negativos.js
@@ -0,0 +1,11 @@
+var numero = parseint(prompt("Digite um numero"));
+
+if(numero > 0){
+    console.log(" o numero é positivo");
+}else if(numero < 0){
+    console.log("O numero é negativo");
+}else if(numero == 0){
+    console.log("O numero é 0");
+}else{
+    console.log("Digite um número ! burro!");
+}
