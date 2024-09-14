b4849ea4b296f9604bcc64f9a867d87d8d046674
Matheus00017 <vidalmatheus321@gmail.com>
2024-09-13 22:01:53 -0300

    números pares e impares

---
+++ numeros_pares_e_impares.js
@@ -0,0 +1,10 @@
+var numero = parseInt(prompt("Digite um numero:"));
+ 
+
+if(numero % 2 == 0 ){
+console.log("O número: " + numero + " é par");
+}else if(numero){
+    console.log("O número: " + numero +" é impar");
+}else{
+    console.log("Digite um número");
+}
